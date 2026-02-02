c277ec0f6042f9add88b58ac126b1ea882bc4694
digvijay971 <thakaredigvijay00@gmail.com>
2026-02-02 20:47:41 +0530

    Add files via upload

---
+++ lesson1.3/activity1.3.2.py
@@ -0,0 +1,12 @@
+name = "Penguin"
+age = 15
+is_student = True
+weight = 38.5
+print("Name:", name)
+print("Data Type of Name is :", type(name))
+print("Age:", age)
+print("Data Type of Age is :", type(age))
+print("is_student", is_student)
+print("Data Type of is student is", type(is_student))
+print("Weight : ", weight)
+print("Data Type of weight is", type(weight))
